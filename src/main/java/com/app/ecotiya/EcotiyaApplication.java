c7b3b8b02aa4d0d4c277d8325f10c53c5240b281
ecotiya <ghqyydf829@gmail.com>
2022-05-12 22:03:41 +0900

    build.gradle 更新 パッケージ構成変更

---
+++ src/main/java/com/app/ecotiya/EcotiyaApplication.java
@@ -0,0 +1,13 @@
+package com.app.ecotiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcotiyaApplication.class, args);
+	}
+
+}
