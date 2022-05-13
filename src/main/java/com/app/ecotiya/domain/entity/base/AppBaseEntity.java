dde41f9ca9c6d5e92de00176661d3b87d7dba4a6
ecotiya <ghqyydf829@gmail.com>
2022-05-13 13:09:21 +0900

    パッケージ構成を修正

---
+++ src/main/java/com/app/ecotiya/domain/entity/base/AppBaseEntity.java
@@ -0,0 +1,26 @@
+package com.app.ecotiya.domain.entity.base;
+
+import java.time.LocalDateTime;
+
+public class AppBaseEntity {
+    
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    
+    public LocalDateTime getCreatedAt () {
+        return createdAt;
+    }
+    
+    public void setCreatedAt (LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    public LocalDateTime getUpdatedAt () {
+        return updatedAt;
+    }
+    
+    public void setUpdatedAt (LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+}
