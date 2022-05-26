cbbf3cb2615109ea59a251d85b8be507e9b044d0
ecotiya <ghqyydf829@gmail.com>
2022-05-26 18:46:32 +0900

    ロゴをヘッダーの左端に表示すru。/ ヘッダーの色を白に変更

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -2,62 +2,69 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import AppBar from '../atoms/AppBar';
 import Toolbar from '../atoms/Toolbar';
 
+const whiteTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+    },
+  },
+});
+
 const rightLink = {
   fontSize: 25,
-  color: 'common.white',
+  color: 'common.brack',
   ml: 3,
 };
 
 function Header() {
   return (
     <div>
-      <AppBar position="fixed">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          {/* <Box sx={{ flex: 1, justifyContent: 'flex-start' }}>
-            <Link
-              variant="h3"
-              underline="none"
-              color="inherit"
-              href="/"
-              sx={{ fontSize: 24 }}
-            >
-              ecotiya
-            </Link>
-          </Box> */}
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link
-              color="inherit"
-              variant="subtitle1"
-              underline="hover"
-              href="/"
-              sx={rightLink}
-            >
-              Home
-            </Link>
-            <Link
-              color="inherit"
-              variant="subtitle1"
-              underline="hover"
-              href="/about"
-              sx={rightLink}
-            >
-              About
-            </Link>
-            <Link
-              color="inherit"
-              variant="subtitle1"
-              underline="hover"
-              href="/contact"
-              sx={rightLink}
-            >
-              Contact
-            </Link>
-          </Box>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={whiteTheme}>
+        <AppBar position="fixed" color="primary">
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <Box sx={{ flex: 1, justifyContent: 'flex-start' }}>
+              <img
+                src="/logo_transparent.png"
+                alt="ecotiya Logo"
+                width="81px"
+              />
+            </Box>
+            <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+              <Link
+                color="inherit"
+                variant="subtitle1"
+                underline="hover"
+                href="/"
+                sx={rightLink}
+              >
+                Home
+              </Link>
+              <Link
+                color="inherit"
+                variant="subtitle1"
+                underline="hover"
+                href="/about"
+                sx={rightLink}
+              >
+                About
+              </Link>
+              <Link
+                color="inherit"
+                variant="subtitle1"
+                underline="hover"
+                href="/contact"
+                sx={rightLink}
+              >
+                Contact
+              </Link>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
       <Toolbar />
     </div>
   );
