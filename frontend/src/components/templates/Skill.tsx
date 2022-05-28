1eb8d047dab91969ba4f9a08b90ed95964d83402
ecotiya <ghqyydf829@gmail.com>
2022-05-28 14:40:11 +0900

    Skill画面のテーブル構成を修正、経験年数を★で表示できるように対応

--- frontend/src/components/templates/Skill.tsx
+++ frontend/src/components/templates/Skill.tsx
@@ -11,6 +11,8 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { styled } from '@mui/material/styles';
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -22,85 +24,40 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
-// const StyledTableRow = styled(TableRow)(({ theme }) => ({
-//   '&:nth-of-type(odd)': {
-//     backgroundColor: theme.palette.action.hover,
-//   },
-//   // hide last border
-//   '&:last-child td, &:last-child th': {
-//     border: 0,
-//   },
-// }));
-
 interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  id: 'skillName' | 'experienceYm';
   label: string;
   minWidth?: number;
   align?: 'right';
-  format?: (value: number) => string;
+  isExperienceYm?: boolean;
 }
 
 const columns: Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'skillName', label: 'スキル名', minWidth: 170 },
   {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
+    id: 'experienceYm',
+    label: '経験年数',
+    minWidth: 100,
+    isExperienceYm: true,
   },
 ];
 
 interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
+  skillName: string;
+  experienceYm: number;
 }
 
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number,
-): Data {
-  const density = population / size;
-
-  return { name, code, population, size, density };
+function createData(skillName: string, experienceYm: number): Data {
+  return { skillName, experienceYm };
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('Java', 4.0),
+  createData('VB.NET', 2.0),
+  createData('PHP', 0.5),
+  createData('C#', 0.5),
+  createData('PowerShell', 0.5),
+  createData('Perl', 0.5),
 ];
 
 export default function Skill() {
@@ -158,16 +115,30 @@ export default function Skill() {
                         hover
                         role="checkbox"
                         tabIndex={-1}
-                        key={row.code}
+                        key={row.skillName}
                       >
                         {columns.map((column) => {
                           const value = row[column.id];
 
                           return (
                             <TableCell key={column.id} align={column.align}>
-                              {column.format && typeof value === 'number'
-                                ? column.format(value)
-                                : value}
+                              {column.isExperienceYm &&
+                              typeof value === 'number' ? (
+                                <Rating
+                                  name="text-feedback"
+                                  value={value}
+                                  readOnly
+                                  precision={0.5}
+                                  emptyIcon={
+                                    <StarIcon
+                                      style={{ opacity: 0.55 }}
+                                      fontSize="inherit"
+                                    />
+                                  }
+                                />
+                              ) : (
+                                value
+                              )}
                             </TableCell>
                           );
                         })}
