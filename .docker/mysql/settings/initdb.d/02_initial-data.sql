d0a680d0b19a31c9cdda90e0336b48f058b73da2
ecotiya <ghqyydf829@gmail.com>
2022-06-12 23:45:29 +0900

    mysqlのテーブル初期データ追加(Skill)

--- .docker/mysql/settings/initdb.d/02_initial-data.sql
+++ .docker/mysql/settings/initdb.d/02_initial-data.sql
@@ -26,3 +26,21 @@ insert into ecotiya_com.m_section_title(section_kind_code,section_title,section_
   , ('contact','【お問い合わせ】',',ここまで、ご覧いただきありがとうございます。私への問い合わせは、下記フォームをご利用ください。',null,null,'1')
   , ('profile','【私について】','プロフィールのサブタイトル','/assets/ecotiya.png','ecotiya','1')
   , ('skill','【スキルセット】','スキルセットのサブタイトル',null,null,'1');
+
+
+insert into ecotiya_com.m_skill(skill_id,skill_field_code,skill_name,experience_ym) values 
+    (1,'Backend','Java',4.0)
+  , (2,'Backend','VB.NET',2.0)
+  , (3,'Backend','PHP',0.5)
+  , (4,'Backend','C#',0.5)
+  , (5,'Backend','PowerShell',0.5)
+  , (6,'Backend','Perl',0.5)
+  , (7,'Frontend','JavaScript',2.0)
+  , (8,'Frontend','Jquery',2.0)
+  , (9,'Frontend','JSP',1.0)
+  , (10,'Frontend','PHP',0.5)
+  , (11,'Frontend','Perl',0.5)
+  , (12,'Frontend','React.js',0.5)
+  , (13,'DevOps','MySQL',2.0)
+  , (14,'DevOps','PostgreSQL',2.0)
+  , (15,'DevOps','CACHE',1.0);
